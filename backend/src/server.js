6f9c3941939d02e9abd70ad05c9655f726a4fd94
tutorial <mayankvatsal800@gmail.com>
2025-12-10 02:28:23 +0530

    Refactor server.js for improved deployment readiness and static file serving

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import {ENV} from "./lib/env.js"
+import path from "path"
+
+
+const app=express();
+
+const __dirname=path.resolve()
+
+
+app.get("/hello",(req,res)=>{
+    res.status(200).json({msg:"message from api"})
+})
+
+// make our app ready for the deployment
+// connects the backend and frontend it will send the static files of the frontend from dist folder 
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../../frontend","dist","index.html"))
+    })
+
+}
+const PORT = ENV.PORT || 5000;
+app.listen(PORT,()=>console.log(`port is running on ${PORT}`))
